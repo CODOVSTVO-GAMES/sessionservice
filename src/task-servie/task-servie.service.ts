fef6d14b660a19e1b42d12dadb1057a7254f61e3
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-05-20 13:15:25 +0300

    fix ligth session

--- src/task-servie/task-servie.service.ts
+++ src/task-servie/task-servie.service.ts
@@ -11,33 +11,33 @@ export class TaskServieService {
     constructor(
         @InjectRepository(ActiveSession) private activeSessionRepo: Repository<ActiveSession>,
         @InjectRepository(OldSession) private oldSessionRepo: Repository<OldSession>
-    ){}
+    ) { }
 
     @Cron(CronExpression.EVERY_MINUTE)
-    async closeSessionByOffline5min(){
+    async closeSessionByOffline5min() {
         const sessions = await this.findAllSessionByLastActiveLong5minAndIsActive()
-        for( let l = 0; l < sessions.length; l++ ){
+        for (let l = 0; l < sessions.length; l++) {
             sessions[l].isActive = false
             await this.activeSessionRepo.save(sessions[l])
         }
     }
 
     @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-    async closeSessionLongDay(){
+    async closeSessionLongDay() {
         const sessions = await this.findAllSessionByLastActiveLongDAYAndIsActive()
-        for( let l = 0; l < sessions.length; l++ ){
+        for (let l = 0; l < sessions.length; l++) {
             sessions[l].isActive = false
             await this.activeSessionRepo.save(sessions[l])
         }
         //log закрыта сессия открытая сутки!!!! ПОЧЕМУ?
     }
     @Cron(CronExpression.EVERY_5_MINUTES)
-    async movieOldSession(){
+    async movieOldSession() {
         const sessions = await this.findAllSessionByIsActiveFalse()
         await this.saveOldSessionByYongSession(sessions)
     }
 
-    async findAllSessionByIsActiveFalse() : Promise<ActiveSession[]>{
+    async findAllSessionByIsActiveFalse(): Promise<ActiveSession[]> {
         const sessions = await this.activeSessionRepo.find(
             {
                 where: {
@@ -48,21 +48,21 @@ export class TaskServieService {
         return sessions
     }
 
-    async saveOldSessionByYongSession(sessions: Array<ActiveSession>){
-        for(let l = 0; l < sessions.length; l++ ){
+    async saveOldSessionByYongSession(sessions: Array<ActiveSession>) {
+        for (let l = 0; l < sessions.length; l++) {
             const session = sessions[l]
 
             const sessionId = session.sessionId
             const accountId = session.accountId
             const startDate = session.createDate
             const endDate = session.lastActive
-            const length = startDate - endDate
+            const length = endDate - startDate
             await this.createOldSession(sessionId, accountId, length, startDate, endDate)
             await this.activeSessionRepo.delete(session)
         }
     }
 
-    async createOldSession(sessionId: number, accountId: string, length: number, startDate: number, endDate: number){
+    async createOldSession(sessionId: number, accountId: string, length: number, startDate: number, endDate: number) {
         const session = await this.oldSessionRepo.save(
             this.oldSessionRepo.create(
                 {
